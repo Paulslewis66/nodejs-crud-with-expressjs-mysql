b7e490d0f5bc7b8795712e369de95095911f0832
paul-s66 <35817407+paul-s66@users.noreply.github.com>
2021-01-05 19:09:44 +0000

    vds

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 //changed index to login res.render
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Node.js Simple CRUD with Express.js and MySQL Tutorial' });
+  res.render('login', { title: 'Node.js Lewis CRUD with Express.js and MySQL Tutorial' });
 });
 
 module.exports = router;
